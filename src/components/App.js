d83404fa01c1de2720068c07bdf4e2e058b4701a
Gururaj-Malgonda <144808012+Gururaj-Malgonda@users.noreply.github.com>
2023-12-14 13:08:59 +0000

    Changes made for greet using ternory

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [greet, setGreet] = useState("")
+  
+  function handleGreet(e){
+    e.preventDefault()
+    setGreet(e.target.value)
+  }
+
+  
   return (
     <div>
         {/* Do not remove the main div */}
+        
+        <form>
+          <p>Enter your name:</p>
+          <input type="text" onChange={handleGreet}></input>
+
+         {
+           greet!="" && (<p>Hello {greet}!</p>)
+         }
+        </form>
     </div>
   )
 }
